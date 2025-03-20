8f7b1b44cf1725e7a541e4551be8fb6dda5f4c22
RelativeBinary <patrick.bornay@gmail.com>
2025-03-20 13:01:24 +1100

    add merge sort rep

--- stoney-codes-leetcode-problems/fundamental-algorithms/merge-sort.ts
+++ stoney-codes-leetcode-problems/fundamental-algorithms/merge-sort.ts
@@ -29,6 +29,26 @@ const merge = (left: number[], right: number[]) => {
 
 console.log(mergeSort([6, 3, 8, 5, 2]));
 
+// rep 20/03/25
+const mergeSort5 = (nums: number[]): number[] => {
+    if (nums.length <= 1) return nums;
+
+    const mid = Math.floor(nums.length / 2);
+    const left = mergeSort5(nums.slice(0, mid));
+    const right mergeSort5(nums.slice(mid));
+
+    return merge5(left,right);
+}
+
+const merge5 = (left: number[], right: number[]): number[] => {
+    let result: number[] = [], i = 0, j = 0;
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) result.push(left[i++]);
+        else result.push(right[j++]);
+    }
+    return result.concat(left.slice(i).concat(right.slice(j)));
+}
+
 // rep 18/03/25
 const mergeSort4 = (nums: number[]): number[] => {
     if (nums.length <= 1) return nums;
