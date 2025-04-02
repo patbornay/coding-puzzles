ea05b9fe5ce911d444e25b714e1431eafebb536c
RelativeBinary <patrick.bornay@gmail.com>
2025-04-02 17:38:35 +1100

    two pointer rep

--- stoney-codes-leetcode-problems/fundamental-algorithms/two-pointer-sliding-window/sliding-window/lengthOfLongestUniqueSubstring.ts
+++ stoney-codes-leetcode-problems/fundamental-algorithms/two-pointer-sliding-window/sliding-window/lengthOfLongestUniqueSubstring.ts
@@ -15,8 +15,30 @@ const lengthOfLongestUniqueSubstring1 = (s: string): number => {
     }
     return maxLength; // return the longest length found
 }
-console.log(lengthOfLongestUniqueSubstring('abcabcbb')); // output: 3 ('abc')
+console.log(lengthOfLongestUniqueSubstring1('abcabcbb')); // output: 3 ('abc')
 
+// rep 02/02/25
+const lengOfLongestUniqSubStr2 = (s: string): number => {
+    const charSet = new Set<string>(); // Store unique characters 
+    let left = 0, maxLength = 0; // Left pointer which moves across the input s and maxLeng for the obv
+
+    // left is the 'lagging' pointer
+    for (let right = 0; right < s.length; right++) { // iterate over the input s
+        // if charSet already has right, continously delete from charSet from where left is
+        // left will always be behind right, so if right is like half way through s and hits 
+        // a duplicate then left will keep iterating through s removing everything up till that half way point
+        while (charSet.has(s[right])) { 
+            charSet.delete(s[left]);  
+            left++;
+        }
+        // then once out of the deleting we continue moving the right pointer adding unique characters
+        charSet.add(s[right]);
+        // we tehn update the maxLength comparing the current longest distance 
+        // with the difference between right and left (+1 for 0 based indexing)
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    return maxLength;
+}
 
 // rep 17/03/25
 const lengOfLongestUniqSubStr = (s: string): number => {
@@ -32,4 +54,5 @@ const lengOfLongestUniqSubStr = (s: string): number => {
         maxLength = Math.max(maxLength, right - left + 1) // right - left is how we figure out what number of indexes the window covers 
         // the + 1 is because we are using zero based indexing
     }
+    return maxLength;
 }
