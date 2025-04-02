a1a8deac25c5b72f8d4ebfa889535c0d52b10436
RelativeBinary <patrick.bornay@gmail.com>
2025-04-03 09:43:43 +1100

    dfs rep

--- stoney-codes-leetcode-problems/fundamental-algorithms/depth-first-search/dfs-reps.ts
+++ stoney-codes-leetcode-problems/fundamental-algorithms/depth-first-search/dfs-reps.ts
@@ -1,5 +1,3 @@
-
-// rep 17/03/25
 const dfs1 = (graph: Map<number, number[]>, start: number): number[] => {
     const stack: number[] = [start];
     const visited = new Set<number>();
@@ -15,4 +13,22 @@ const dfs1 = (graph: Map<number, number[]>, start: number): number[] => {
         }
     }
     return result
+}
+
+// rep 2 3/3/25
+const dfs2 = (graph: Map<number, number[]>, start: number): number[] => {
+    const stack: number[] = [start];
+    const visited = new Set<number>();
+    const result: number[] = [];
+
+    while(stack.length > 0) {
+        const node = stack.pop()!;
+        if (visited.has(node)) continue;
+        visited.add(node);
+        result.push(node);
+        for (const neighbor of graph.get(node) || [])  {
+            if(!visited.has(neighbor)) stack.push(neighbor);
+        }
+    }
+    return result;
 }
