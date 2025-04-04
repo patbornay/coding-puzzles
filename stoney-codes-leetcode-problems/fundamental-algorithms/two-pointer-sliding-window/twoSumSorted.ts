dbb5b39025903af4f90d37499e1cfe496c70449f
RelativeBinary <patrick.bornay@gmail.com>
2025-04-05 07:34:30 +1100

    two sum sorted rep

--- stoney-codes-leetcode-problems/fundamental-algorithms/two-pointer-sliding-window/twoSumSorted.ts
+++ stoney-codes-leetcode-problems/fundamental-algorithms/two-pointer-sliding-window/twoSumSorted.ts
@@ -1,3 +1,22 @@
+const twoSumSorted2 = (nums: number[], target: number): number[] => {
+    let left = 0, right = nums.length -1 ;
+
+    // traverse the nums, left incrementing through and right decrementing through
+    // if the sum of left and right is the target return
+    // if the sum < target move left up otherwise move right down 
+    // this works bc nums is sorted, if it wasnt we wouldnt know which pointer to move 
+    // bc we cant say moving a pointer will bring us closer to the target sum
+    // this way we only traverse the array once.
+    while (left < right) {
+        const sum = nums[left] + nums[right];
+        if (sum === target) return [nums[left], nums[right]];
+        if (sum < target) left++;
+        else right --;
+    }
+
+    return []
+}
+
 const twoSumSorted1 = (nums: number[], target: number): number[] => {
     let left = 0, right = nums.length - 1;
 
