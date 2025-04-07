21310b002a1ce1c97316c09546e594e8e5c30d54
RelativeBinary <patrick.bornay@gmail.com>
2025-04-07 20:39:59 +1000

    add most freq element rep

--- stoney-codes-leetcode-problems/fundamental-algorithms/hashmaps-freq-counting/mostFreqElement.ts
+++ stoney-codes-leetcode-problems/fundamental-algorithms/hashmaps-freq-counting/mostFreqElement.ts
@@ -1,3 +1,23 @@
+// rep 2 7/4/25
+const mostFreqElement2 = (nums: number[]): number => {
+    const freqMap: Record<number, number> = {};
+
+    // create counts
+    for (const num of nums) {
+        freqMap[num] = (freqMap[num] || 0) + 1;
+    }
+
+    // find the max value
+    let maxCount = 0, result = -1;
+    for (const [key, value] of Object.entries(freqMap)) {
+        if (value > maxCount) {
+            maxCount = value;
+            result = Number(key);
+        }
+    }
+    return result;
+}
+
 // rep 1 6/4/25
 const mostFreqElement1 = (nums: number[]): number => {
     const freqMap: Record<number, number> = {};
