ba3ca809dceaa7770c42661b7e628b1fb6126896
RelativeBinary <patrick.bornay@gmail.com>
2025-04-14 07:35:28 +1000

    binary search rep

--- stoney-codes-leetcode-problems/fundamental-algorithms/binary-search.ts
+++ stoney-codes-leetcode-problems/fundamental-algorithms/binary-search.ts
@@ -19,6 +19,19 @@ function binarySearch(nums, target) {
     return -1;
 }
 
+// rep 19 12/4/25
+const binarySearch19 = (nums: number[], target: number): number => {
+    let left = 0, right = nums.length -1;
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);    
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+
 // rep 29/03/25
 const binarySearch18 = (nums: number[], target: number): number => {
     let left = 0, right = nums.length - 1;
