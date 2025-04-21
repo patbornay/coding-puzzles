d495051d98680d306d56943f5d9f89a36acc2347
RelativeBinary <patrick.bornay@gmail.com>
2025-04-21 10:22:52 +1000

    problem rep

---
+++ stoney-codes-leetcode-problems/problems/arrays/two-lists-max-values.ts
@@ -0,0 +1,6 @@
+// consider two lists of ints A and B both of length n.
+// if there is an index i such that A[i] >= A[k] for all k
+// AND B[i] >= B[k] for all k, then return i.
+// Otherwise return -1.
+
+// 1. Understand the problem 
