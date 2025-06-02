cca9dce0e93cdc93c04e449acab0387c7788818f
RelativeBinary <patrick.bornay@gmail.com>
2025-06-03 08:25:05 +1000

    twosum rep

--- stoney-codes-leetcode-problems/problems/arrays/two-sum.ts
+++ stoney-codes-leetcode-problems/problems/arrays/two-sum.ts
@@ -18,73 +18,73 @@
 // • Only one valid answer exists
 
 // optimal strat
-// build a map of complements with index locations for example: nums = [7,9,3] target 10 
-// 7 needs 3 to make ten 
+// build a map of complements with index locations for example: nums = [7,9,3] target 10
+// 7 needs 3 to make ten
 // check map if we have a 3 mapped? nope
 // make key value pair, key 7 and point at index for 7
-// so when we hit 3 and need to find the 7 to make 10 
+// so when we hit 3 and need to find the 7 to make 10
 // we check the map and see that there is a 7 at index 0
-// return index for 3 and index for 7 [2, 0] 
+// return index for 3 and index for 7 [2, 0]
 
-// optimised solution 
+// optimised solution
 function twoSum(nums: number[], target: number): number[] {
-    let numsMap: Map<number, number> = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (numsMap[complement] !== undefined) {
-            return [numsMap[complement], i];
-        }
-        numsMap[nums[i]] = i;
+  let numsMap: Map<number, number> = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numsMap[complement] !== undefined) {
+      return [numsMap[complement], i];
     }
-    return [];
+    numsMap[nums[i]] = i;
+  }
+  return [];
 }
 
 // rep 3 03/06/25
 function twoSum3(nums: number[], target: number): number[] {
-    // setup a dictionary where the key is the number and the value is the index 
-    let numsMap: Map<number, number> = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
+  // setup a dictionary where the key is the number and the value is the index
+  let numsMap: Map<number, number> = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
 
-        // if map has the necessary complement to make target return
-        if (numsMap[complement] !== undefined) {
-            return [numsMap[complement], i];
-        }
-
-        // otherwise add this number to the map
-        numsMap[nums[i]] = i;
+    // if map has the necessary complement to make target return
+    if (numsMap[complement] !== undefined) {
+      return [numsMap[complement], i];
     }
-    return [];
+
+    // otherwise add this number to the map
+    numsMap[nums[i]] = i;
+  }
+  return [];
 }
 
 // rep 2 31/5/25
 function twoSum2(nums: number[], target: number): number[] {
-    let numsMap: Map<number, number> = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (numsMap[complement] !== undefined) {
-            return [numsMap[complement], i];
-        }
-        numsMap[nums[i]] = i;
+  let numsMap: Map<number, number> = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numsMap[complement] !== undefined) {
+      return [numsMap[complement], i];
     }
-    return [];
+    numsMap[nums[i]] = i;
+  }
+  return [];
 }
 
 // rep 1 23/5/25
 function twoSum1(nums: number[], target: number): number[] {
-    // setup a map that has a num in nums as the key and its index as the value
-    let numsMap: Map<number, number> = new Map();
+  // setup a map that has a num in nums as the key and its index as the value
+  let numsMap: Map<number, number> = new Map();
 
-    // walk through nums, creating those mappings everytime we don't find a pair to make our target value
-    // pair - given the current number we are looking at in nums, what sum would we need to reach target
-    // if we do find the pair that would land give us the target value, 
-    // then return its value along with the index of the current number we are looking at
-    for (let i = 0; i < nums.length; ++i) {
-        const complement = target - nums[i];
-        if (numsMap[complement] !== undefined) {
-            return [numsMap[complement], i];
-        }
-        numsMap[nums[i]] = i;
+  // walk through nums, creating those mappings everytime we don't find a pair to make our target value
+  // pair - given the current number we are looking at in nums, what sum would we need to reach target
+  // if we do find the pair that would land give us the target value,
+  // then return its value along with the index of the current number we are looking at
+  for (let i = 0; i < nums.length; ++i) {
+    const complement = target - nums[i];
+    if (numsMap[complement] !== undefined) {
+      return [numsMap[complement], i];
     }
-    return [];
+    numsMap[nums[i]] = i;
+  }
+  return [];
 }
