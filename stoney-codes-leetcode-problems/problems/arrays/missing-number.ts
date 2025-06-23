ed38cfe0bb05e7ba5524ad50ddf126310f9ea990
RelativeBinary <patrick.bornay@gmail.com>
2025-06-24 08:16:07 +1000

    missing number reps

--- stoney-codes-leetcode-problems/problems/arrays/missing-number.ts
+++ stoney-codes-leetcode-problems/problems/arrays/missing-number.ts
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/missing-number/description/
+// all numbers are unique 
 
 // input: nums = [3,0,1]
 // output: 2
@@ -32,9 +33,73 @@
 //   this would also catch duplicates
 
 const missingNums = (nums: number[]): number => {
-    const sortedNums: number[] = nums.sort(); // todo check this sorts correctly
+    const sortedNums: number[] = nums.sort((a,b) => a - b); // todo check this sorts correctly
+    for (let i = 0; i < nums.length; i++) {
+        if (sortedNums[i] !== i) return i; // the missing num is the index
+    }
+    return nums.length;
+}
+
+const missingNumsOptimised = (nums: number[]): number => {
+    let missingNum = nums.length;
+    for (let i = 0; i < nums.length; i++) {
+        missingNum += i - nums[i];
+        // basically if we know 3 is the length then by subtracting all the numbs 
+        // we should get 0 if all nums are present [0,1,2] 
+        // --> (i:0 - nums[0]: 0 = 0, ) 
+        // --> (i:1 - nums[1]: 1 = 0, ) 
+        // --> (i:2 - nums[2]: 2 = 0, ) 
+        // therefore res never changes and remains the init value of 3, now we know 3 is missing
+    }
+    return missingNum;
+}
+
+const missingNumsOptimised1 = (nums: number[]): number => {
+    let missingNum = nums.length;
+    for (let i = 0; i < nums.length; i++) {
+        missingNum += i - nums[i];
+        // we subtract nums[i] from i and then add that difference to nums.len
+        // if all nums exist expect nums.len then all the sums will cancel out and 
+        // we end up with nums.len, otherwise we end up with the number that is missing 
+        // e.g. nums = [0,1,2], i(0) - 0 = 0 >> 3 + 0 = 3, i(1) - 1 = 0 >> 3 + 0 = 0,
+        // i(2) - 2 = 0 >> 3 + 0 = 3.
+    }
+    return missingNum;
+}
+
+const missingNums5 = (nums: number[]): number => {
+    // sort so that index must match number, catching missing numbers
+    const sortedNums: number[] = nums.sort((a,b) => a - b);
+    // iterate to identify missing numbers
+    for (let i = 0; i < nums. length; i++) {
+        if (sortedNums[i] !== i) return i; 
+    }
+    return nums.length; // bc if all are provided matching index then the only number missing is n
+}
+
+const missingNums4 = (nums: number[]): number => {
+    // sort so that index must match number, catching missing numbers 
+    const sortedNums: number[] = nums.sort((a,b) => a - b);
+    // iterate to identify missing numbers
     for (let i = 0; i < nums.length; i++) {
         if (sortedNums[i] !== i) return i;
+    }
+    return nums.length;
+}
+
+const missingNUms3 = (nums: number[]): number => {
+    const sortedNums: number[] = nums.sort((a,b) => a - b);
+    for (let i = 0; i < nums.length; i++) {
+        if (sortedNums[i] !== i) return i;
+    }
+    return nums.length;
+}
+
+const missingNums2 = (nums: number[]): number => {
+    const sortedNums: number[] = nums.sort();
+    for (let i = 0; i < nums.length; i++) {
+        // by sorting the array we can state the i will always === sortedNums[i] if it is not missing
+        if (sortedNums[i] !== i) return i; 
     }
     return nums.length;
 }
