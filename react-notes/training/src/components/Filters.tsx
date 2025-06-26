ce24ffbaad32eae0191083d6557d033c33df9f7a
RelativeBinary <patrick.bornay@gmail.com>
2025-06-27 08:25:01 +1000

    improve page layout spacing

--- react-notes/training/src/components/Filters.tsx
+++ react-notes/training/src/components/Filters.tsx
@@ -1,5 +1,23 @@
-import styles from './Filters.module.css';
+import styles from "./Filters.module.css";
 
 export const Filters = () => {
-  return <div className={styles["filters"]}> This is filters </div>;
-}
+  return (
+    <div className={styles["filters"]}>
+      <h4>Filters</h4>
+      <div className="hourly-rate-section">
+        <h5>Hourly rate</h5>
+        <a>Clear</a>
+        <div className="min-max-container">
+          <div>min</div>
+          <div>to</div>
+          <div>max</div>
+        </div>
+      </div>
+      <div className="countries-section">
+        <h5>Countries</h5>
+        <a href="">Clear</a>
+        <input />
+      </div>
+    </div>
+  );
+};
