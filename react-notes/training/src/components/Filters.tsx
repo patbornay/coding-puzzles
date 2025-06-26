8aca98cc9ff832999de45bc7e6ec52261299d8e6
RelativeBinary <patrick.bornay@gmail.com>
2025-06-27 08:55:36 +1000

    filter styling

--- react-notes/training/src/components/Filters.tsx
+++ react-notes/training/src/components/Filters.tsx
@@ -4,18 +4,30 @@ export const Filters = () => {
   return (
     <div className={styles["filters"]}>
       <h4>Filters</h4>
-      <div className="hourly-rate-section">
-        <h5>Hourly rate</h5>
-        <a>Clear</a>
-        <div className="min-max-container">
-          <div>min</div>
+      <div className={styles["hourly-rate-section"]}>
+        <div className={styles["hourly-rate__heading"]}>
+          <h5>Hourly rate</h5>
+          <p>
+            <a href="">Clear</a>
+          </p>
+        </div>
+        <div className={styles["hourly-rate__min-max-controls"]}>
+          <div>
+            <input />
+          </div>
           <div>to</div>
-          <div>max</div>
+          <div>
+            <input />
+          </div>
         </div>
       </div>
-      <div className="countries-section">
-        <h5>Countries</h5>
-        <a href="">Clear</a>
+      <div className={styles["countries-section"]}>
+        <div className={styles["countries__heading"]}>
+          <h5>Countries</h5>
+          <p>
+            <a href="">Clear</a>
+          </p>
+        </div>
         <input />
       </div>
     </div>
