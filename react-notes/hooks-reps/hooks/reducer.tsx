f91be325da9ee7c69d5e54c0af2ccc991af6599a
RelativeBinary <patrick.bornay@gmail.com>
2025-07-16 23:11:35 +1000

    reducer rep

--- react-notes/hooks-reps/hooks/reducer.tsx
+++ react-notes/hooks-reps/hooks/reducer.tsx
@@ -24,3 +24,25 @@ const Counter = () => {
 }
 
 // reps go here
+const myReducer1 = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      return state;
+  }
+}
+
+const Counter1 = () => {
+  const [state, dispatch] = useReducer(myReducer, {count: 0});
+
+  return (
+    <div>
+      <p>Count {state.count}</p>
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+    </div>
+  )
+}
