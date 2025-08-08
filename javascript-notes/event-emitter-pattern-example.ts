84bb11d6ec8cb7fd3074ca1db60c9195fb06a1ad
RelativeBinary <patrick.bornay@gmail.com>
2025-08-09 01:18:13 +1000

    event emmitter example

---
+++ javascript-notes/event-emitter-pattern-example.ts
@@ -0,0 +1,32 @@
+// 5. EVENT EMITTER PATTERN
+// Demonstrates observer pattern implementation
+
+class EventEmitter {
+  events: any = null;
+
+  constructor() {
+    this.events = {};
+  }
+  
+  on(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+  }
+  
+  emit(event, data) {
+    if (this.events[event]) {
+      this.events[event].forEach(callback => callback(data));
+    }
+  }
+  
+  off(event, callback) {
+    if (this.events[event]) {
+      this.events[event] = this.events[event].filter(cb => cb !== callback);
+    }
+  }
+}
+
+const emitter = new EventEmitter();
+emitter.on('user-login', (user) => console.log(`${user.name} logged in`));
