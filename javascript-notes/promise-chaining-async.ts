35263c88cc49b361078e9d97fe0345f42fef608a
RelativeBinary <patrick.bornay@gmail.com>
2025-08-17 20:40:13 +1000

    examples

--- javascript-notes/promise-chaining-async.ts
+++ javascript-notes/promise-chaining-async.ts
@@ -5,16 +5,16 @@ export const profileProvider = () => {
     async getProfile() {
       if (cachedData) return { data: cachedData };
       if (inProgress) return inProgress;
-      inProgress = fetch('https://jsonplaceholder.typicode.com/users/1')
-        .then(res => {
+      inProgress = fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then((res) => {
           if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
           return res.json();
         })
-        .then(data => {
+        .then((data) => {
           cachedData = data;
           return { data };
         })
-        .catch(err => {
+        .catch((err) => {
           return { error: err };
         })
         .finally(() => {
@@ -33,19 +33,19 @@ const promise1 = profileService.getProfile();
 const promise2 = profileService.getProfile();
 const promise3 = profileService.getProfile();
 
-// Analysis reps 
+// Analysis reps
 /**
- * Notes/Improvements 
- * - use async await pattern to avoid promise chaining 
- * - why do we use cache? it is so we dont call getprofile all the time for the same data 
+ * Notes/Improvements
+ * - use async await pattern to avoid promise chaining
+ * - why do we use cache? it is so we dont call getprofile all the time for the same data
  * - why do we have inProgress? it is so we dont getprofile while it is already being fetched
  * - use a clear cache method for the user to make a new req without getting old data
  */
 
-// Reps go here 
+// Reps go here
 /**
- * Write a function that: 
- * - Provides a method to getProfile which makes an API call 
+ * Write a function that:
+ * - Provides a method to getProfile which makes an API call
  * to https://jsonplaceholder.typicode.com/users/1
  * - prevents calling while a call is inProgress
  * - contains a cache to store already fetched data
@@ -57,7 +57,7 @@ const getProfile = () => {
   let cachedData: any = null;
   return {
     async getProfile() {
-      if (cachedData) return { data: cachedData};
+      if (cachedData) return { data: cachedData };
       if (inProgress) return inProgress;
 
       inProgress = fetch("Some URL")
@@ -67,16 +67,44 @@ const getProfile = () => {
         })
         .then((data) => {
           cachedData = data;
-          return {data};
+          return { data };
         })
         .catch((err) => {
-          return {error: err};
+          return { error: err };
         })
         .finally(() => {
           inProgress = null;
         });
 
-      return inProgress
-    }
-  }
-}
+      return inProgress;
+    },
+  };
+};
+
+const getProfile1 = () => {
+  let inProgress: any = null;
+  let cachedData: any = null;
+  return {
+    async getProfile() {
+      if (cachedData) return { data: cachedData };
+      if (inProgress) return inProgress;
+
+      inProgress = fetch("some url")
+        .then((res) => {
+          if (!res.ok) throw new Error("Error occurred");
+          return res.json();
+        })
+        .then((data) => {
+          cachedData = data;
+          return { data };
+        })
+        .catch((err) => {
+          return { error: err };
+        })
+        .finally(() => {
+          inProgress = null;
+        });
+      return inProgress;
+    },
+  };
+};
